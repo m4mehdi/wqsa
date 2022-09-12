5a811cb5e03b338ad924f49ff8713e2481aa3132
m4mehdi <m3mehdi@hotmail.com>
2022-09-12 18:56:21 +0430

    Delete web_project directory

--- web_project/web_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'web_api'
--- web_project/web_api/producer.py
+++
@@ -1,8 +0,0 @@
-import pika
-
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
-channel = connection.channel()
-
-def publish(body):
-    channel.basic_publish(exchange='', routing_key='qos', body=body)
